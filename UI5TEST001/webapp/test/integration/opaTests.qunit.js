2672e5a43925cbb178af7b209af1cbd49343239c
welcome-rain <db.im@readians.com>
2020-03-16 10:39:23 +0000

    Add HTML5 Module

---
+++ UI5TEST001/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"com/readians/com/UI5TEST001/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
---
+++ UI5TEST001/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/readians/com/UI5TEST001/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
